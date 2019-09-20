6dc44441033ec50f542836ca9e09ab8df47c61d0
Chanuka Dissanayaka <chanuka04heshan@gmail.com>
2019-09-20 11:42:26 +0530

    unit test commit

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AppService } from './app.service';
 
-describe('AppService', () => {
+fdescribe('AppService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
   let service: AppService;
 
